1e6ec54b7b94e55d10b2f1bdd2a9c5875fd26b18
yashwanthreddyg <yashwanthreddy.g@live.com>
2016-08-14 14:32:13 +0530

    completed the basic functionalities

--- app.py
+++ app.py
@@ -1,47 +1,83 @@
-import os,time
-# from guessit import guessit
+# -*- coding: utf-8 -*-
+import os,time,urllib,json,collections
+import shutil
+from utils import *
+from urllib import FancyURLopener
+from guessit import guessit
+
+class MyOpener(FancyURLopener):
+    version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'
 
 def addons(folderPath,recursive = False):
-    #print os.path.isdir('./');
-    # for d in dirList:
-    #     if os.path.isdir(d) == True:
-    #         stat = os.stat(d)
-    #         created = os.stat(d).st_mtime
-    #         asciiTime = time.asctime( time.gmtime( created ) )
-    #         print d, "is a dir  (created", asciiTime, ")"
     if not os.path.isdir(folderPath):
         return False
-    files = getOnlyFileNames(folderPath,recursive)
-    for f in files:
-        print f[1]
+    files = getAllFileNames(folderPath,recursive)
+    finalFiles = []
+    for file in files:
+        if isMediaFileName(file[0]):
+            finalFiles.append(file)
+    fileDict = {}
+    for f in finalFiles:
+        fileDict[f[1]] ={}
+        guessed = guessit(f[0])
+        fileDict[f[1]]['file_details'] = guessed
+    fileDict = getDetails(fileDict)
+    
+    print "writing to files"
+    for entry in fileDict:
+        if not "movie_details" in fileDict[entry]:
+            print "no movie details for "+entry
+            continue
+        if fileDict[entry]["movie_details"]['Response'] == u'False':
+            continue
+
+        parentFolderPath = os.path.dirname(entry);
+        try:
+            name = entry[:entry.rfind('.')]
+            writeFile = open(name+".minfo","w+")
+            for item in fileDict[entry]["movie_details"]:
+                decoded = fileDict[entry]["movie_details"][item].encode('ascii', 'ignore').decode('ascii')
+                writeFile.write(item + " : " + decoded + "\n")
+            writeFile.close()
+            posterData = fileDict[entry].pop("poster_data",None)
+            if posterData != None:
+                writePoster = open(name+".jpg","wb+");
+                writePoster.write(posterData);
+                writePoster.close()
+            
+        except Exception as e:
+            print e
+        
+    print "done with writing to files"
             
     return True
-def getOnlyFileNames(folderPath,recursive = False):
-    if recursive:
-        return getFileNamesRecursive(folderPath)
-    else:
-        return getFileNames(folderPath)
-def getFileNames(folderPath):
-    if not os.path.isdir(folderPath):
-        return None
-    entries = os.listdir(folderPath)
-    files = []
-    for entry in entries:
-        absPath = os.path.abspath(os.path.join(folderPath,entry))
-        if not os.path.isdir(absPath):
-            files.append((entry,absPath))
-    return files
-def getFileNamesRecursive(folderPath):
-    if not os.path.isdir(folderPath):
-        return None
-    files = []
-    for entry in os.listdir(folderPath):
-        absPath = os.path.abspath(os.path.join(folderPath,entry))
-        if os.path.isdir(absPath):
-            files.extend(getFileNamesRecursive(absPath))
-        else:
-             files.append((entry,absPath))
-    return files
+
+def getDetails(filesDict):
+    for file in filesDict:
+        fileDetails = filesDict[file]['file_details']
+        try:
+            if "title" in fileDetails:
+                url = "http://www.omdbapi.com/?" + "t=" + fileDetails['title'] + "&tomatoes=true&plot=full"
+                print "retrieving \""+ fileDetails["title"] + "\" details"
+                response = urllib.urlopen(url).read()
+                print "retrieved details of \""+ fileDetails['title'] + "\""
+                jsonresponse = json.JSONDecoder(object_pairs_hook=collections.OrderedDict).decode(response)
+                filesDict[file]["movie_details"] = jsonresponse
+                posterURL = jsonresponse.pop(u"Poster",None)
+                if posterURL != None and posterURL!=u"N/A":
+                    print "retrieving \""+ fileDetails["title"] + "\" poster"
+                    posterData = urllib.urlopen(posterURL)
+                    filesDict[file]["poster_data"] = posterData.read()
+                    print "retrieved poster of \""+ fileDetails['title']+ "\""
+        except Exception as e:
+            print "check connectivity"
+            print e
+    return filesDict
+
 if __name__ == "__main__":
     print "Hello World"
-    addons("D:\\testFiles",True)
+    folderPath = "D:/testFiles"
+
+    folderPath = folderPath.decode("utf-8")
+    asciiFolderPath = folderPath.encode('ascii','ignore')
+    addons(folderPath,True)
---
+++ utils.py
@@ -0,0 +1,94 @@
+import os
+mediaExtensions = \
+    ['264', '3G2', '3GP', '3GP2', '3GPP', '3GPP2', '3MM', '3P2',
+    '60D', '787', '890', 'AAF', 'AEC', 'AEP', 'AEPX', 'AET',
+    'AETX', 'AJP', 'ALE', 'AM', 'AMC', 'AMV', 'AMX', 'ANIM', 
+    'ANX', 'AQT', 'ARCUT', 'ARF', 'ASF', 'ASX', 'AVB', 'AVC',
+    'AVCHD', 'AVD', 'AVI', 'AVM', 'AVP', 'AVS', 'AVS', 'AVV', 
+    'AWLIVE', 'AXM', 'AXV', 'BDM', 'BDMV', 'BDT2', 'BDT3', 'BIK',
+    'BIN', 'BIX', 'BMC', 'BMK', 'BNP', 'BOX', 'BS4', 'BSF', 
+    'BU', 'BVR', 'BYU', 'CAMPROJ', 'CAMREC', 'CAMV', 'CED', 'CEL',
+    'CINE', 'CIP', 'CLK', 'CLPI', 'CMMP', 'CMMTPL', 'CMPROJ', 'CMREC',
+    'CMV', 'CPI', 'CPVC', 'CST', 'CVC', 'CX3', 'D2V', 'D3V',
+    'DASH', 'DAT', 'DAV', 'DB2', 'DCE', 'DCK', 'DCR', 'DCR',
+    'DDAT', 'DIF', 'DIR', 'DIVX', 'DLX', 'DMB', 'DMSD', 'DMSD3D',
+    'DMSM', 'DMSM3D', 'DMSS', 'DMX', 'DNC', 'DPA', 'DPG', 'DREAM',
+    'DSY', 'DV', 'DV-AVI', 'DV4', 'DVDMEDIA', 'DVR', 'DVR-MS', 'DVX',
+    'DXR', 'DZM', 'DZP', 'DZT', 'EDL', 'EVO', 'EVO', 'EXO',
+    'EYE', 'EYETV', 'EZT', 'F4F', 'F4P', 'F4V', 'FBR', 'FBR',
+    'FBZ', 'FCARCH', 'FCP', 'FCPROJECT', 'FFD', 'FFM', 'FLC', 'FLH',
+    'FLI', 'FLV', 'FLX', 'FPDX', 'FTC', 'G64', 'GCS', 'GFP',
+    'GIFV', 'GL', 'GOM', 'GRASP', 'GTS', 'GVI', 'GVP', 'GXF',
+    'H264', 'HDMOV', 'HDV', 'HKM', 'IFO', 'IMOVIELIBRARY', 'IMOVIEMOBILE', 'IMOVIEPROJ',
+    'IMOVIEPROJECT', 'INP', 'INT', 'IRCP', 'IRF', 'ISM', 'ISMC',
+    'ISMCLIP', 'ISMV', 'IVA', 'IVF', 'IVR', 'IVS', 'IZZ', 'IZZY',
+    'JMV', 'JSS', 'JTS', 'JTV', 'K3G', 'KDENLIVE', 'KMV', 'KTN',
+    'LREC', 'LRV', 'LSF', 'LSX', 'LVIX', 'M15', 'M1PG', 'M1V',
+    'M21', 'M21', 'M2A', 'M2P', 'M2T', 'M2TS', 'M2V', 'M4E',
+    'M4U', 'M4V', 'M75', 'MANI', 'META', 'MGV', 'MJ2', 'MJP',
+    'MJPEG', 'MJPG', 'MK3D', 'MKV', 'MMV', 'MNV', 'MOB', 'MOD',
+    'MODD', 'MOFF', 'MOI', 'MOOV', 'MOV', 'MOVIE', 'MP21', 'MP21',
+    'MP2V', 'MP4', 'MP4.INFOVID', 'MP4V', 'MPE', 'MPEG', 'MPEG1',
+    'MPEG2', 'MPEG4', 'MPF', 'MPG', 'MPG2', 'MPG4', 'MPGINDEX', 'MPL',
+    'MPL', 'MPLS', 'MPROJ', 'MPSUB', 'MPV', 'MPV2', 'MQV', 'MSDVD',
+    'MSE', 'MSH', 'MSWMM', 'MT2S', 'MTS', 'MTV', 'MVB', 'MVC',
+    'MVD', 'MVE', 'MVEX', 'MVP', 'MVP', 'MVY', 'MXF', 'MXV',
+    'MYS', 'NCOR', 'NSV', 'NTP', 'NUT', 'NUV', 'NVC', 'OGM',
+    'OGV', 'OGX', 'ORV', 'OSP', 'OTRKEY', 'PAC', 'PAR', 'PDS',
+    'PGI', 'PHOTOSHOW', 'PIV', 'PJS', 'PLAYLIST', 'PLPROJ', 'PMF', 'PMV',
+    'PNS', 'PPJ', 'PREL', 'PRO', 'PRO4DVD', 'PRO5DVD', 'PROQC', 'PRPROJ',
+    'PRTL', 'PSB', 'PSH', 'PSSD', 'PVA', 'PVR', 'PXV', 'QT',
+    'QTCH', 'QTINDEX', 'QTL', 'QTM', 'QTZ', 'R3D', 'RCD', 'RCPROJECT',
+    'RCREC', 'RCUT', 'RDB', 'REC', 'RM', 'RMD', 'RMD', 'RMP',
+    'RMS', 'RMV', 'RMVB', 'ROQ', 'RP', 'RSX', 'RTS', 'RTS',
+    'RUM', 'RV', 'RVID', 'RVL', 'SAN', 'SBK', 'SBT', 'SBZ',
+    'SCC', 'SCM', 'SCM', 'SCN', 'SCREENFLOW', 'SDV', 'SEC', 'SEC',
+    'SEDPRJ', 'SEQ', 'SFD', 'SFERA', 'SFVIDCAP', 'SIV', 'SMI', 'SMI',
+    'SMIL', 'SMK', 'SML', 'SMV', 'SNAGPROJ', 'SPL', 'SQZ',
+    'SSF', 'SSM', 'STL', 'STR', 'STX', 'SVI', 'SWF', 'SWI',
+    'SWT', 'TDA3MT', 'TDT', 'TDX', 'THEATER', 'THP', 'TID',
+    'TIVO', 'TIX', 'TOD', 'TP', 'TP0', 'TPD', 'TPR', 'TREC',
+    'TRP', 'TS', 'TSP', 'TTXT', 'TVLAYER', 'TVRECORDING', 'TVS', 'TVSHOW',
+    'USF', 'USM', 'VBC', 'VC1', 'VCPF', 'VCR', 'VCV', 'VDO',
+    'VDR', 'VDX', 'VEG', 'VEM', 'VEP', 'VF', 'VFT', 'VFW',
+    'VFZ', 'VGZ', 'VID', 'VIDEO', 'VIEWLET', 'VIV', 'VIVO', 'VIX',
+    'VLAB', 'VMLF', 'VMLT', 'VOB', 'VP3', 'VP6', 'VP7', 'VPJ',
+    'VRO', 'VS4', 'VSE', 'VSP', 'VTT', 'W32', 'WCP', 'WEBM',
+    'WFSP', 'WGI', 'WLMP', 'WM', 'WMD', 'WMMP', 'WMV', 'WMX',
+    'WOT','WP3', 'WPL', 'WSVE', 'WTV', 'WVE', 'WVX', 'WXP',
+    'XEJ', 'XEL', 'XESC', 'XFL', 'XLMV', 'XML', 'XMV', 'XVID',
+    'Y4M', 'YOG', 'YUV', 'ZEG', 'ZM1', 'ZM2', 'ZM3', 'ZMV']
+
+def getAllFileNames(folderPath,recursive = False):
+    if recursive:
+        return getFileNamesRecursive(folderPath)
+    else:
+        return getFileNames(folderPath)
+def getFileNames(folderPath):
+    if not os.path.isdir(folderPath):
+        return None
+    entries = os.listdir(folderPath)
+    files = []
+    for entry in entries:
+        absPath = os.path.abspath(os.path.join(folderPath,entry))
+        if not os.path.isdir(absPath):
+            files.append((entry,absPath))
+    return files
+def getFileNamesRecursive(folderPath):
+    if not os.path.isdir(folderPath):
+        return None
+    files = []
+    for entry in os.listdir(folderPath):
+        absPath = os.path.abspath(os.path.join(folderPath,entry))
+        if os.path.isdir(absPath):
+            files.extend(getFileNamesRecursive(absPath))
+        else:
+             files.append((entry,absPath))
+    return files
+def isMediaFileName(fileName):
+    if not '.' in fileName:
+        return False
+    fileExtension = fileName.split('.')[-1:][0]
+    if fileExtension.upper() in mediaExtensions:
+        return True
+    return False
